144c931a2058fb6e1c33b8fcc3d3a9497d187260
Melvil GUILLAUME <melvil.guillaume@gmail.com>
2017-11-21 20:41:01 +0100

    added list

--- termux.py
+++ termux.py
@@ -18,3 +18,37 @@ def camera_info():
     if rc:
         raise Exception(err)
     return out
+
+
+# termux-camera-photo
+# termux-clipboard-get
+# termux-clipboard-set
+# termux-contact-list
+# termux-dialog
+# termux-download
+# termux-fix-shebang
+# termux-info
+# termux-infrared-frequencies
+# termux-infrared-transmit
+# termux-location
+# termux-notification
+# termux-notification-remove
+# termux-open
+# termux-open-url
+# termux-reload-settings
+# termux-setup-storage
+# termux-share
+# termux-sms-inbox
+# termux-sms-send
+# termux-storage-get
+# termux-telephony-call
+# termux-telephony-cellinfo
+# termux-telephony-deviceinfo
+# termux-toast
+# termux-tts-engines
+# termux-tts-speak
+# termux-vibrate
+# termux-wake-lock
+# termux-wake-unlock
+# termux-wifi-connectioninfo
+# termux-wifi-scaninfo
