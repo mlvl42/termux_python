fe7f27655bd5f5c88deb40ff8f077f2998e2e476
mlvl42 <melvil.guillaume@gmail.com>
2018-10-09 16:50:20 +0200

    fix shameful error caused by my vim macro

--- termux.py
+++ termux.py
@@ -28,119 +28,119 @@ def termux_camera_photo():
 
 
 def termux_clipboard_get():
-    out, rc, err = utils.execute('termux-clipboard-gt')
+    out, rc, err = utils.execute('termux-clipboard-get')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_clipboard_set():
-    out, rc, err = utils.execute('termux-clipboard-st')
+    out, rc, err = utils.execute('termux-clipboard-set')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_contact_list():
-    out, rc, err = utils.execute('termux-contact-lit')
+    out, rc, err = utils.execute('termux-contact-list')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_dialog():
-    out, rc, err = utils.execute('termux-dialg')
+    out, rc, err = utils.execute('termux-dialog')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_download():
-    out, rc, err = utils.execute('termux-downlod')
+    out, rc, err = utils.execute('termux-download')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_fix_shebang():
-    out, rc, err = utils.execute('termux-fix-shebag')
+    out, rc, err = utils.execute('termux-fix-shebang')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_info():
-    out, rc, err = utils.execute('termux-ino')
+    out, rc, err = utils.execute('termux-info')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_infrared_frequencies():
-    out, rc, err = utils.execute('termux-infrared-frequencis')
+    out, rc, err = utils.execute('termux-infrared-frequencies')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_infrared_transmit():
-    out, rc, err = utils.execute('termux-infrared-transmt')
+    out, rc, err = utils.execute('termux-infrared-transmit')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_location():
-    out, rc, err = utils.execute('termux-locatin')
+    out, rc, err = utils.execute('termux-location')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_notification():
-    out, rc, err = utils.execute('termux-notificatin')
+    out, rc, err = utils.execute('termux-notification')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_notification_remove():
-    out, rc, err = utils.execute('termux-notification-remoe')
+    out, rc, err = utils.execute('termux-notification-remove')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_open():
-    out, rc, err = utils.execute('termux-opn')
+    out, rc, err = utils.execute('termux-open')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_open_url():
-    out, rc, err = utils.execute('termux-open-ul')
+    out, rc, err = utils.execute('termux-open-url')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_reload_settings():
-    out, rc, err = utils.execute('termux-reload-settins')
+    out, rc, err = utils.execute('termux-reload-settings')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_setup_storage():
-    out, rc, err = utils.execute('termux-setup-storae')
+    out, rc, err = utils.execute('termux-setup-storage')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_share():
-    out, rc, err = utils.execute('termux-shae')
+    out, rc, err = utils.execute('termux-share')
     if rc:
         raise Exception(err)
     return out
@@ -154,91 +154,91 @@ def termux_sms_inbox():
 
 
 def termux_sms_send():
-    out, rc, err = utils.execute('termux-sms-sed')
+    out, rc, err = utils.execute('termux-sms-send')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_storage_get():
-    out, rc, err = utils.execute('termux-storage-gt')
+    out, rc, err = utils.execute('termux-storage-get')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_telephony_call():
-    out, rc, err = utils.execute('termux-telephony-cal')
+    out, rc, err = utils.execute('termux-telephony-call')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_telephony_cellinfo():
-    out, rc, err = utils.execute('termux-telephony-cellino')
+    out, rc, err = utils.execute('termux-telephony-cellinfo')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_telephony_deviceinfo():
-    out, rc, err = utils.execute('termux-telephony-deviceino')
+    out, rc, err = utils.execute('termux-telephony-deviceinfo')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_toast():
-    out, rc, err = utils.execute('termux-toat')
+    out, rc, err = utils.execute('termux-toast')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_tts_engines():
-    out, rc, err = utils.execute('termux-tts-engins')
+    out, rc, err = utils.execute('termux-tts-engines')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_tts_speak():
-    out, rc, err = utils.execute('termux-tts-spek')
+    out, rc, err = utils.execute('termux-tts-speak')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_vibrate():
-    out, rc, err = utils.execute('termux-vibrae')
+    out, rc, err = utils.execute('termux-vibrate')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_wake_lock():
-    out, rc, err = utils.execute('termux-wake-lok')
+    out, rc, err = utils.execute('termux-wake-lock')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_wake_unlock():
-    out, rc, err = utils.execute('termux-wake-unlok')
+    out, rc, err = utils.execute('termux-wake-unlock')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_wifi_connectioninfo():
-    out, rc, err = utils.execute('termux-wifi-connectionino')
+    out, rc, err = utils.execute('termux-wifi-connectioninfo')
     if rc:
         raise Exception(err)
     return out
 
 
 def termux_wifi_scaninfo():
-    out, rc, err = utils.execute('termux-wifi-scanino')
+    out, rc, err = utils.execute('termux-wifi-scaninfo')
     if rc:
         raise Exception(err)
     return out
