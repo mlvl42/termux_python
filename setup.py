dd3ef41578400bbec6ce85469a4f75b260690497
mlvl42 <melvil.guillaume@gmail.com>
2022-07-27 17:42:30 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='termux_python-mguillau42',
+    name='termux_python',
     version='0.0.1',
-    author='mguillau42',
+    author='mlvl42',
     description='termux python bindings',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/mguillau42/termux_python',
+    url='https://github.com/mlvl42/termux_python',
     packages=setuptools.find_packages(),
     classifiers= [
        'Programming Language :: Python :: 3',
