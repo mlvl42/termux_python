8ed9f60a81714f69077b1f5ea0d6cc78102eb2c0
mlvl42 <melvil.guillaume@gmail.com>
2022-07-28 15:27:00 +0200

    feat!: parameters handled for most apis
    Renders most of termux APIs useable from python and improves
    error handling. Adds also the support for some new APIs.

--- termux/termux.py
+++ termux/termux.py
@@ -1,244 +1,419 @@
-import subprocess
+import json
+from typing import Dict, List, Optional, Union
+
 from termux.common import utils
 
 
-"""
-    implementation of all the termux-api commands
-    via subprocesses,
-"""
-def battery_status():
-    out, rc, err = utils.execute('termux-battery-status')
+class PermissionError(Exception):
+    pass
+
+
+class ApiError(Exception):
+    pass
+
+
+def api_wrapper(cmd: Union[str, List]) -> str:
+
+    # ensure all params are str before passing to subprocess
+    if isinstance(cmd, list):
+        cmd = list(map(str, cmd))
+
+    out, rc, err = utils.execute(cmd)
+
     if rc:
-        raise Exception(err)
+        print(cmd)
+        print(out, rc, err)
+        raise ApiError(err)
+    elif "API_ERROR" in out:
+        error = json.loads(out)["API_ERROR"]
+        raise ApiError(error)
+
+    if "Please grant the following permission to use this command" in out:
+        error = json.loads(out)["error"]
+        raise PermissionError(error)
+
     return out
 
 
-def camera_info():
-    out, rc, err = utils.execute('termux-camera-info')
-    if rc:
-        raise Exception(err)
-    return out
+def api_start():
+    api_wrapper("termux-api-start")
 
 
-def camera_photo():
-    out, rc, err = utils.execute('termux-camera-photo')
-    if rc:
-        raise Exception(err)
-    return out
+def api_stop():
+    api_wrapper("termux-api-stop")
 
 
-def clipboard_get():
-    out, rc, err = utils.execute('termux-clipboard-get')
-    if rc:
-        raise Exception(err)
-    return out
+def api_restart():
+    api_wrapper("termux-api-stop")
+    api_wrapper("termux-api-start")
 
 
-def clipboard_set():
-    out, rc, err = utils.execute('termux-clipboard-set')
-    if rc:
-        raise Exception(err)
-    return out
+def audio_info() -> Dict:
+    return json.loads(api_wrapper("termux-audio-info"))
 
 
-def contact_list():
-    out, rc, err = utils.execute('termux-contact-list')
-    if rc:
-        raise Exception(err)
-    return out
+def battery_status() -> Dict:
+    return json.loads(api_wrapper("termux-battery-status"))
 
 
-def dialog():
-    out, rc, err = utils.execute('termux-dialog')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def brightness(level: int):
+#     pass
 
 
-def download():
-    out, rc, err = utils.execute('termux-download')
-    if rc:
-        raise Exception(err)
-    return out
+def call_log(limit: Optional[int] = None, offset: Optional[int] = None) -> List[Dict]:
+    cmd = ["termux-call-log"]
+
+    if limit:
+        cmd += ["-l", limit]
+    if offset:
+        cmd += ["-o", offset]
+
+    return json.loads(api_wrapper(cmd))
 
 
-def fix_shebang():
-    out, rc, err = utils.execute('termux-fix-shebang')
-    if rc:
-        raise Exception(err)
-    return out
+def camera_info() -> List[Dict]:
+    return json.loads(api_wrapper("termux-camera-info"))
 
 
-def info():
-    out, rc, err = utils.execute('termux-info')
-    if rc:
-        raise Exception(err)
-    return out
+def camera_photo(output_file: str, camera_id: Optional[str] = None):
+    cmd = ["termux-camera-photo"]
+
+    if camera_id:
+        cmd += ["-c", camera_id]
+
+    cmd.append(output_file)
+
+    return api_wrapper(cmd)
+
+
+def clipboard_get() -> str:
+    return api_wrapper("termux-clipboard-get")
+
+
+def clipboard_set(text: str):
+    api_wrapper(["termux-clipboard-set", text])
+
+
+def contact_list() -> List[Dict]:
+    return json.loads(api_wrapper("termux-contact-list"))
+
+
+# TODO
+# def dialog():
+#     pass
+
+
+def download(
+    url: str,
+    desc: Optional[str] = None,
+    title: Optional[str] = None,
+    path: Optional[str] = None,
+):
+    cmd = ["termux-download"]
+
+    if desc:
+        cmd += ["-d", desc]
+
+    if title:
+        cmd += ["-t", title]
+
+    if path:
+        cmd += ["-p", path]
+
+    cmd.append(url)
+
+    return api_wrapper(cmd)
+
+
+def fingerprint(
+    title: Optional[str] = None,
+    desc: Optional[str] = None,
+    subtitle: Optional[str] = None,
+    cancel: Optional[str] = None,
+) -> Dict:
+    cmd = ["termux-fingerprint"]
+
+    if title:
+        cmd += ["-t", title]
+
+    if desc:
+        cmd += ["-d", desc]
+
+    if subtitle:
+        cmd += ["-s", subtitle]
+
+    if cancel:
+        cmd += ["-c", cancel]
+
+    return json.loads(api_wrapper(cmd))
 
 
 def infrared_frequencies():
-    out, rc, err = utils.execute('termux-infrared-frequencies')
-    if rc:
-        raise Exception(err)
-    return out
+    return json.loads(api_wrapper("termux-infrared-frequencies"))
 
 
-def infrared_transmit():
-    out, rc, err = utils.execute('termux-infrared-transmit')
-    if rc:
-        raise Exception(err)
-    return out
+def infrared_transmit(frequency: int, pattern: str):
+    return api_wrapper(["termux-infrared-transmit", "-f", frequency, pattern])
 
 
-def location():
-    out, rc, err = utils.execute('termux-location')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def job_scheduler():
+#     pass
 
 
-def notification():
-    out, rc, err = utils.execute('termux-notification')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def keystore():
+#     pass
 
 
-def notification_remove():
-    out, rc, err = utils.execute('termux-notification-remove')
-    if rc:
-        raise Exception(err)
-    return out
+def location(provider: Optional[str] = None, request: Optional[str] = None) -> Dict:
+    cmd = ["termux-location"]
+
+    if provider:
+        cmd += ["-p", provider]
+
+    if request:
+        cmd += ["-r", request]
+
+    return json.loads(api_wrapper(cmd))
 
 
-def open():
-    out, rc, err = utils.execute('termux-open')
-    if rc:
-        raise Exception(err)
-    return out
+def media_player(command: str, args: Optional[List[str]] = None) -> str:
+    cmd = ["termux-media-player", command]
+
+    if args:
+        cmd += args
+
+    return api_wrapper(cmd)
 
 
-def open_url():
-    out, rc, err = utils.execute('termux-open-url')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def media_scan():
+#     pass
 
 
-def reload_settings():
-    out, rc, err = utils.execute('termux-reload-settings')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def microphone_record():
+# pass
 
 
-def setup_storage():
-    out, rc, err = utils.execute('termux-setup-storage')
-    if rc:
-        raise Exception(err)
-    return out
+def nfc_read(short: bool = True):
+    cmd = ["termux-nfc", "-r", "short" if short else "full"]
+    return api_wrapper(cmd)
 
 
-def share():
-    out, rc, err = utils.execute('termux-share')
-    if rc:
-        raise Exception(err)
-    return out
+def nfc_write(text: str):
+    cmd = ["termux-nfc", "-w", text]
+    return api_wrapper(cmd)
 
 
-def sms_inbox():
-    out, rc, err = utils.execute('termux-sms-inbox')
-    if rc:
-        raise Exception(err)
-    return out
+def notification_list() -> List[Dict]:
+    return json.loads(api_wrapper("termux-notification-list"))
 
 
-def sms_send():
-    out, rc, err = utils.execute('termux-sms-send')
-    if rc:
-        raise Exception(err)
-    return out
+def notification_remove(id: int) -> str:
+    return api_wrapper(["termux-notification-remove", id])
 
 
-def storage_get():
-    out, rc, err = utils.execute('termux-storage-get')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def notification():
+#     pass
 
 
-def telephony_call():
-    out, rc, err = utils.execute('termux-telephony-call')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO
+# def saf_*():
+# pass
 
 
-def telephony_cellinfo():
-    out, rc, err = utils.execute('termux-telephony-cellinfo')
-    if rc:
-        raise Exception(err)
-    return out
+def open(
+    path: str,
+    send: bool = False,
+    chooser: bool = False,
+    content_type: Optional[str] = None,
+):
+    cmd = ["termux-open"]
+
+    if send:
+        cmd.append("--send")
+    else:
+        cmd.append("--view")
+
+    if chooser:
+        cmd.append("--chooser")
+
+    if content_type:
+        cmd += ["--content-type", content_type]
+
+    cmd.append(path)
+    api_wrapper(cmd)
 
 
-def telephony_deviceinfo():
-    out, rc, err = utils.execute('termux-telephony-deviceinfo')
-    if rc:
-        raise Exception(err)
-    return out
+def sensor_list() -> Dict:
+    return json.loads(api_wrapper(["termux-sensor", "-l"]))
 
 
-def toast():
-    out, rc, err = utils.execute('termux-toast')
-    if rc:
-        raise Exception(err)
-    return out
+# TODO: stream command output
+# def sensor_listen(sensors: Union[str,List[str]], delay: Optional[int] = None) -> Dict:
+#     cmd = ['termux-sensor']
+
+#     if sensors == "all":
+#         cmd.append('-a')
+#     elif isinstance(sensors, list):
+#         cmd += ['-s', ','.join(sensors)]
+#     else:
+#         raise ValueError('Unexpected value for sensors param')
+
+#     if delay:
+#         cmd += ['-d', delay]
+
+#     return json.loads(api_wrapper(['termux-sensor', '-l']))
 
 
-def tts_engines():
-    out, rc, err = utils.execute('termux-tts-engines')
-    if rc:
-        raise Exception(err)
-    return out
+def sms_list(
+    limit: Optional[int] = None,
+    offset: Optional[int] = None,
+    type: Optional[str] = None,
+    conversation_list: bool = False,
+    number: Optional[str] = None,
+) -> List[Dict]:
+    cmd = ["termux-sms-list"]
+
+    if limit:
+        cmd += ["-l", limit]
+    if offset:
+        cmd += ["-o", offset]
+    if type:
+        cmd += ["-t", type]
+    if conversation_list:
+        cmd.append("-c")
+
+    if number:
+        cmd += ["-f", number]
+
+    return json.loads(api_wrapper(cmd))
 
 
-def tts_speak():
-    out, rc, err = utils.execute('termux-tts-speak')
-    if rc:
-        raise Exception(err)
-    return out
+def sms_send(text: str, number: Union[str, List[str]], slot: int = 0):
+    cmd = ["termux-sms-send", "-s", slot]
+
+    if isinstance(number, str):
+        cmd += ["-n", number]
+    else:
+        cmd += ["-n", ",".join(number)]
+
+    cmd.append(text)
+
+    api_wrapper(cmd)
 
 
-def vibrate():
-    out, rc, err = utils.execute('termux-vibrate')
-    if rc:
-        raise Exception(err)
-    return out
+def speech_to_text() -> str:
+    return api_wrapper("termux-speech-to-text")
 
 
-def wake_lock():
-    out, rc, err = utils.execute('termux-wake-lock')
-    if rc:
-        raise Exception(err)
-    return out
+def storage_get(output_file: str):
+    api_wrapper(["termux-storage-get", output_file])
 
 
-def wake_unlock():
-    out, rc, err = utils.execute('termux-wake-unlock')
-    if rc:
-        raise Exception(err)
-    return out
+def telephony_call(number: str):
+    api_wrapper(["termux-telephony-call", number])
+
+
+def telephony_cellinfo() -> List[Dict]:
+    return json.loads(api_wrapper(["termux-telephony-cellinfo"]))
+
+
+def telephony_deviceinfo() -> Dict:
+    return json.loads(api_wrapper(["termux-telephony-deviceinfo"]))
+
+
+def toast(
+    text: str,
+    bgcolor: Optional[str] = None,
+    color: Optional[str] = None,
+    gravity: Optional[str] = None,
+    short: bool = False,
+):
+    cmd = ["termux-toast"]
+
+    if bgcolor:
+        cmd += ['-b', bgcolor]
+    if color:
+        cmd += ['-c', color]
+    if gravity:
+        cmd += ['-g', gravity]
+    if short:
+        cmd.append('-s')
+
+    cmd.append(text)
+
+    api_wrapper(cmd)
+
+
+def torch(enabled: bool):
+    api_wrapper(['termux-torch', "on" if enabled else "off"])
+
+
+def tts_engines() -> List[Dict]:
+    return json.loads(api_wrapper('termux-tts-engines'))
+
+
+def tts_speak(
+    text: str,
+    engine: Optional[str] = None,
+    lang: Optional[str] = None,
+    region: Optional[str] = None,
+    variant: Optional[str] = None,
+    pitch: Optional[float] = None,
+    rate: Optional[float] = None,
+    stream: Optional[str] = None,
+):
+
+    cmd = ["termux-tts-speak"]
+
+    if engine:
+        cmd += ["-e", engine]
+    if lang:
+        cmd += ["-l", lang]
+    if region:
+        cmd += ["-n",region]
+    if variant:
+        cmd += ["-v",variant]
+    if pitch:
+        cmd += ["-p",pitch]
+    if rate:
+        cmd += ["-r",rate]
+    if stream:
+        cmd += ["-s",stream]
+
+    cmd.append(text)
+
+    api_wrapper(cmd)
+
+
+def vibrate(duration: Optional[int] = None, force: bool = False):
+    cmd = ['termux-vibrate']
+
+    if duration:
+        cmd += ['-d', duration]
+
+    if force:
+        cmd.append('-f')
+
+    api_wrapper(cmd)
+
+
+def volume(stream: str, value: int):
+   api_wrapper(['termux-volume', stream, value])
 
 
 def wifi_connectioninfo():
-    out, rc, err = utils.execute('termux-wifi-connectioninfo')
-    if rc:
-        raise Exception(err)
-    return out
+   return json.loads(api_wrapper('termux-wifi-connectioninfo'))
+
+
+def wifi_enable(enabled: bool):
+   api_wrapper(['termux-wifi-enable', 'true' if enabled else 'false'])
 
 
 def wifi_scaninfo():
-    out, rc, err = utils.execute('termux-wifi-scaninfo')
-    if rc:
-        raise Exception(err)
-    return out
+   return json.loads(api_wrapper('termux-wifi-scaninfo'))
